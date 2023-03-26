9f048e8294cfcab8e14d3847a8c1f0747ec8b454
Sarthak Gupta <61643495+SarthakGupta2912@users.noreply.github.com>
2023-03-26 18:42:13 +0530

    UpdatedCode

--- Test.java
+++ Test.java
@@ -1,3 +1,41 @@
+import java.util.Scanner;
+
 public class Test {
-  
+
+    public static void main(String[] args) {
+        
+        String userString;
+        Scanner scObj = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        userString = scObj.nextLine();
+
+        CheckVal(userString, userString + " is an integer!", userString + " is not an integer!", userString + " is an integer but with a greater length!");
+    }
+
+    static void CheckVal(String userString, String messageIfCorrect, String messageIfNotNumber, String messageIfNumberExceedsLength) {
+
+        try {
+            Integer.valueOf(userString);
+            System.out.println(messageIfCorrect);
+        } catch (NumberFormatException e) {
+
+            int containChar = 0;
+            for (int i = 0; i <= userString.length() - 1; i++) {
+                if (Character.isDigit(userString.charAt(i)) == false) {
+                    containChar = 1;
+                    break;
+                }
+            }
+            if (containChar == 1) {
+                System.out.println(messageIfNotNumber);
+                //CheckVal();
+            } else {
+                System.out.println(messageIfNumberExceedsLength);
+                //CheckVal();
+            }
+
+        }
+    }
+
 }
